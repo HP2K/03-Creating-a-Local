7fd22ca2ae3f88429351c4fa146746023d5dd2da
Hans Pelle Jart <hanspellejart@gmail.com>
2021-04-18 13:34:36 +0200

    updated code

--- LS2Server.js
+++ LS2Server.js
@@ -1,11 +1,9 @@
 // Express to run the server and routes
 const express = require ('express');
-
 // Start up instace of app
 const app = express ();
-
 /* Dependencies */
-const bodyParser = require ('body-parser') 
+const bodyParser = require ('body-parser');
 /* Middleware */
 // Here we ate configuring express to use bodyParser as middleware //
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,10 +11,8 @@ app.use(bodyParser.json());
 // Cors for cross origion allowance
 const cors = require('cors');
 app.use(cors());
-
 //inniziate the main project folder//
 app.use(express.static('demo'));
-
 const port = 8000;
 // Spin up the server
 const server = app.listen(port, listening);
