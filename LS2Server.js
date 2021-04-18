89076c0ab433ca539d8a38033140a9aed894ee12
Hans Pelle Jart <hanspellejart@gmail.com>
2021-04-18 08:38:13 +0200

    transfer

---
+++ LS2Server.js
@@ -0,0 +1,28 @@
+// Express to run the server and routes
+const express = require ('express');
+
+// Start up instace of app
+const app = express ();
+
+/* Dependencies */
+const bodyParser = require ('body-parser') 
+/* Middleware */
+// Here we ate configuring express to use bodyParser as middleware //
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Cors for cross origion allowance
+const cors = require('cors');
+app.use(cors());
+
+//inniziate the main project folder//
+app.use(express.staic('demo'));
+
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+// const server = app.listen(port, ()=>(console.log(`running on localhost: ${port}`)})
+// Callback to debug
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
